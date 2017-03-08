7824c1fcb863088d624b059d910767e5144d4cdf
Neelmani.Karn <neelmani.karn@trantorinc.com>
2017-03-08 19:02:50 +0530

    test

--- app/src/main/java/com/trantorinc/mvpsamplewithfragments/activity/BaseActivity.java
+++ app/src/main/java/com/trantorinc/mvpsamplewithfragments/activity/BaseActivity.java
@@ -12,5 +12,7 @@ public class BaseActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //Test for git
     }
 }
